c9e7cdc8988e0e9578e9998963c621e5d53df0c1
shirisha24-hub <ammusiri242@gmail.com>
2023-11-21 00:17:54 +0300

    lab4

---
+++ contacts_app/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
